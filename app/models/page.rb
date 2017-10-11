adfe73a314119e6873e2ab34d9732cd91d8c1c69
John Hartman <john.f.d.hartman@gmail.com>
2017-10-11 10:48:22 -0700

    replaced destroy method with delete method

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,12 +1,12 @@
 class Page < ApplicationRecord
 
-  def self.destroy_meta_pages
+  def self.delete_meta_pages
       #destroys all pages that begin with Category:, Wikipedia:,
       #MediaWiki: or contain (disambiguation)
-      Page.where("
+      Page.delete(Page.where("
         title SIMILAR TO
           '(Wikipedia\:|Category\:|MediaWiki\:|Portal\:)%'
         OR title SIMILAR TO '%disambiguation%'")
-        .destroy
+        .map {|page| page.id})
   end
 end
